6afce136573ac41839a0f63434747f3b0835bf30
John Jermanis <jermanis@yahoo.com>
2022-03-28 15:05:21 -0700

    Error handling mode: reprompt all guesses if no options remain

--- wordle-solver/InteractiveGame.cs
+++ wordle-solver/InteractiveGame.cs
@@ -17,6 +17,8 @@ namespace wordle_solver
         public void PlayGame()
         {
             var options = new PossibleWords(_words, GUESS_COUNT);
+            var guesses = new string[6];
+
             Console.WriteLine("Welcome to wordle-solver. This program will help you solve the daily Wordle puzzle.");
             Console.WriteLine("To use, guess what this program suggests. Then, let this program know the result.");
             Console.WriteLine("Enter the five colors from the result. G for green, Y for yellow, and X for gray.");
@@ -24,8 +26,15 @@ namespace wordle_solver
             for (int i = 0; i < GUESS_COUNT; i++)
             {
                 var currGuess = options.BestGuess();
-                Console.WriteLine($"Please guess: {currGuess}");
-                var result = PromptResult();
+
+                if (currGuess == null)
+                {
+                    options = ReplayGuesses(guesses);
+                    currGuess = options.BestGuess();
+                }
+
+                guesses[i] = currGuess;
+                var result = PromptResult(currGuess);
                 if (result == "GGGGG")
                 {
                     Console.WriteLine("Congrats!");
@@ -36,8 +45,9 @@ namespace wordle_solver
             Console.WriteLine("Looks like you ran out of guesses. My fault.");
         }
 
-        private static string PromptResult()
+        private static string PromptResult(string word)
         {
+            Console.WriteLine($"Please guess: {word}");
             while (true)
             {
                 Console.Write("Result? ");
@@ -47,6 +57,28 @@ namespace wordle_solver
                 else
                     return result;
             }
+        }
+
+        private PossibleWords ReplayGuesses(string[] previousGuesses)
+        {
+            Console.WriteLine("Hmmm. I do not see any valid guesses for you. Maybe this happened because you");
+            Console.WriteLine("made a mistake entering in the results after a guess. I'm going to ask for the");
+            Console.WriteLine("results for each guess again. Please enter each result and we'll see if it helps.");
+
+            var options = new PossibleWords(_words, GUESS_COUNT);
+            foreach (var guess in previousGuesses)
+            {
+                if (guess == null)
+                    break;
+                var result = PromptResult(guess);
+                options.UpdateGuess(guess, result);
+            }
+            if (options.BestGuess() == null)
+            {
+                Console.WriteLine("I still do not have a guess for you. Sorry - I might have a bug.");
+                throw new Exception();
+            }
+            return options;
         }
     }
 }
