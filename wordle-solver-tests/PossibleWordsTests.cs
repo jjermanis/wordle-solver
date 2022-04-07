8a1a3777a88f9a26d1841f61aeec731d7e3f2200
John Jermanis <jermanis@yahoo.com>
2022-04-06 17:20:50 -0700

    Unit tests for expected results

--- wordle-solver-tests/PossibleWordsTests.cs
+++ wordle-solver-tests/PossibleWordsTests.cs
@@ -17,5 +17,30 @@ namespace wordle_solver_tests
         {
             Assert.IsTrue(PossibleWords.IsWordValid("tutor", "XXGXG", "meter"));
         }
+
+        [TestMethod]
+        public void CalcResult_Speed_Abide()
+        {
+            Assert.AreEqual("XXYXY", PossibleWords.CalcResult("speed", "abide"));
+        }
+
+        [TestMethod]
+        public void CalcResult_Speed_Erase()
+        {
+            Assert.AreEqual("YXYYX", PossibleWords.CalcResult("speed", "erase"));
+        }
+
+        [TestMethod]
+        public void CalcResult_Speed_Steal()
+        {
+            Assert.AreEqual("GXGXX", PossibleWords.CalcResult("speed", "steal"));
+        }
+
+        [TestMethod]
+        public void CalcResult_Speed_Crepe()
+        {
+            Assert.AreEqual("XYGYX", PossibleWords.CalcResult("speed", "crepe"));
+        }
+
     }
 }
